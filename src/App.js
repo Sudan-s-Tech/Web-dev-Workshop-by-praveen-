7264b309954cbc30e34566062e6c67e4f7ef3403
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-08-02 11:51:25 +0530

    App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Skills from "./components/Skills";
+import Intro from "./components/Intro";
+import Work from "./components/Work";
+import Contactme from "./components/Contactme";
+import Config from "./config.json";
 
 function App() {
-  return (<div></div>);
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <table>
+          <tbody>
+            <Intro Data={Config.Intro} />
+            <Skills Data={Config.Skills} />
+            <Work Data={Config.Work} />
+            <Contactme />
+          </tbody>
+        </table>
+      </div>
+      <div id="snackbar"></div>
+      <div id="snackbar2"></div>
+    </>
+  );
 }
 
 export default App;
