82bb7110bd5c3cf431f6950d3ea06ab5a68e488a
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-08-02 17:59:16 +0530

    Update header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Header = () => {
+  let count = 0;
   return (
     <header>
       <nav className="nav" id="nav">
@@ -12,7 +13,27 @@ const Header = () => {
         <a href="#" className="active">
           DOWNLOAD CV <i className="fa fa-download"></i>
         </a>
-        <a className="icon" id="icon">
+        <a
+          className="icon"
+          id="icon"
+          onClick={
+            //NAVIGATION
+
+            function opennav() {
+              if (count === 0) {
+                document.getElementById("nav").style.height = "260px";
+                document.getElementById("icon").style.color = "#3399ff";
+                document.getElementById("nav").style.borderRadius = "0px";
+                count++;
+              } else {
+                document.getElementById("nav").style.height = "45px";
+                document.getElementById("icon").style.color = "#000";
+                document.getElementById("nav").style.borderRadius = "40px";
+                count = 0;
+              }
+            }
+          }
+        >
           <i className="fa fa-bars"></i>
         </a>
       </nav>
